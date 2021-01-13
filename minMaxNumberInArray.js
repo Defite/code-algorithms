e03237e2b1b3679777dc5b7172d76566a2e3c4c9
Nikita Makhov <defite@gmail.com>
2021-01-13 13:36:32 +0300

    Update minMaxNumberInArray.js

--- minMaxNumberInArray.js
+++ minMaxNumberInArray.js
@@ -16,16 +16,16 @@ for (let i = 1; i < numbers.length; ++i) {
   }
 }
 
-// 2. Apply
-Math.min.apply(Math, numbers);
-Math.max.apply(Math, numbers);
-
 let min = numbers[0];
 for (let i = 1; i < numbers.length; ++i) {
   if (numbers[i] < min) {
       min = numbers[i];
   }
 }
+
+// 2. Apply
+Math.min.apply(Math, numbers);
+Math.max.apply(Math, numbers);
 
 // 3. ES6
 Math.min(...numbers);
